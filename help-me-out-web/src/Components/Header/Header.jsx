41f85eb2c5f82b391fd313e7e62c846470b45e46
Jesse-Maurice Iyoha <iyohajessy@gmail.com>
2023-10-02 15:55:58 +0100

    routing done

--- help-me-out-web/src/Components/Header/Header.jsx
+++ help-me-out-web/src/Components/Header/Header.jsx
@@ -1,16 +1,20 @@
 const Header = () => {
   return (
-    <header className='lg:px-[100px] py-[12px] shadow top-0 bg-white'>
+    <header className="lg:px-[100px] py-[12px] shadow top-0 bg-white">
       <nav className="flex items-center justify-between">
-        <img src="/images/logo.svg" alt="logo" />
+        <a href="/">
+          <img src="/images/logo.svg" alt="logo" />
+        </a>
         <div className="flex gap-[39px]">
-          <span className='font-medium font-work'>Features</span>
-          <span className='font-medium font-work'>How It Works</span>
+          <span className="font-medium font-work">Features</span>
+          <span className="font-medium font-work">How It Works</span>
         </div>
-        <span className="font-sora font-semibold text-[18px] text-primary-main"><a href="/login"></a>Get Started</span>
+        <span className="font-sora font-semibold text-[18px] text-primary-main">
+          <a href="/login">Get Started</a>
+        </span>
       </nav>
     </header>
-  )
+  );
 }
 
 export default Header
--- help-me-out-web/src/pages/LoginPage/Login.jsx
+++ help-me-out-web/src/pages/LoginPage/Login.jsx
@@ -1,57 +1,85 @@
-import "./Login.css";
-import "../global.css"
-
+import './Login.css';
+import '../global.css';
 
 const Login = () => {
     return (
-        <div className="sign-uplogin">
-            <div className="nav">
-                <div className="logo">
-                    <div className="icon-7-1-parent">
-                        <img className="icon-7-1" alt="" src="/icons/icons-pack/android-chrome-192x192.png" />
-                        <b className="helpmeout">HelpMeOut</b>
-                    </div>
-                </div>
+      <div className="sign-uplogin">
+        <div className="nav">
+          <div className="logo">
+            <a href="/">
+              <div className="icon-7-1-parent">
+                <img
+                  className="icon-7-1"
+                  alt=""
+                  src="/icons/icons-pack/android-chrome-192x192.png"
+                />
+                <b className="helpmeout">HelpMeOut</b>
+              </div>
+            </a>
+          </div>
+        </div>
 
+        <div className="frame-container">
+          <div className="log-in-or-sign-up-parent">
+            <b className="log-in-or w-color">Log in or Sign up</b>
+            <div className="join-millions-of-container">
+              <span className="join-millions-of">{`Join millions of others in sharing successful moves on `}</span>
+              <span className="helpmeout w-color">HelpMeOut</span>
+              <span className="join-millions-of">.</span>
             </div>
-
-            <div className="frame-container">
-                <div className="log-in-or-sign-up-parent">
-                    <b className="log-in-or w-color">Log in or Sign up</b>
-                    <div className="join-millions-of-container">
-                        <span className="join-millions-of">{`Join millions of others in sharing successful moves on `}</span>
-                        <span className="helpmeout w-color">HelpMeOut</span>
-                        <span className="join-millions-of">.</span>
-                    </div>
+          </div>
+          <div className="login-sso1-parent">
+            <button className="login-sso1">
+              <img
+                className="google-svg-icon"
+                alt=""
+                src="/icons/help_assets/Google svg.svg"
+              />
+              <div className="continue-with-google w-color">
+                Continue with Google
+              </div>
+            </button>
+            <button className="login-sso1">
+              <img
+                className="google-svg-icon"
+                alt=""
+                src="/icons/help_assets/Facebook svg.svg"
+              />
+              <div className="continue-with-google w-color">
+                Continue with Facebook
+              </div>
+            </button>
+          </div>
+          <div className="vector-parent">
+            <img
+              className="frame-child"
+              alt=""
+              src="/icons/help_assets/Line 18.svg"
+            />
+            <div className="or-wrapper">
+              <div className="continue-with-google w-color"> or </div>
+            </div>
+            <img
+              className="frame-child"
+              alt=""
+              src="/icons/help_assets/Line 18.svg"
+            />
+          </div>
+          <div className="inputfield-parent">
+            <div className="login-inputfield">
+              <div className="baseinputfield2">
+                <div className="title2 w-color">Email</div>
+                <div className="user-container">
+                  <img className="user-icon2" alt="" src="/user2.svg" />
+                  <div className="input-text-here2">
+                    <input
+                      type="email"
+                      placeholder="Enter your email address"
+                    />
+                  </div>
+                  <img className="user-icon2" alt="" src="/danger2.svg" />
                 </div>
-                <div className="login-sso1-parent">
-                    <button className="login-sso1">
-                        <img className="google-svg-icon" alt="" src="/icons/help_assets/Google svg.svg" />
-                        <div className="continue-with-google w-color">Continue with Google</div>
-                    </button>
-                    <button className="login-sso1">
-                        <img className="google-svg-icon" alt="" src="/icons/help_assets/Facebook svg.svg" />
-                        <div className="continue-with-google w-color">Continue with Facebook</div>
-                    </button>
-
-                </div>
-                <div className="vector-parent">
-                    <img className="frame-child" alt="" src="/icons/help_assets/Line 18.svg" />
-                    <div className="or-wrapper">
-                        <div className="continue-with-google w-color"> or </div>
-                    </div>
-                    <img className="frame-child" alt="" src="/icons/help_assets/Line 18.svg" />
-                </div>
-                <div className="inputfield-parent">
-                    <div className="login-inputfield">
-                        <div className="baseinputfield2">
-                            <div className="title2 w-color">Email</div>
-                            <div className="user-container">
-                                <img className="user-icon2" alt="" src="/user2.svg" />
-                                <div className="input-text-here2"><input type="email" placeholder="Enter your email address"/></div>
-                                <img className="user-icon2" alt="" src="/danger2.svg" />
-                            </div>
-                            {/* <div className="descriptive-text2 w-color">Descriptive text</div>
+                {/* <div className="descriptive-text2 w-color">Descriptive text</div>
                             <div className="frame-div">
                                 <div className="option-1-frame">
                                     <div className="option-12 w-color">Option 1</div>
@@ -63,17 +91,19 @@ const Login = () => {
                                     <div className="option-12">Option 3</div>
                                 </div>
                             </div> */}
-                        </div>
-                    </div>
-                    <div className="login-inputfield">
-                        <div className="baseinputfield2">
-                            <div className="title2 w-color">Password</div>
-                            <div className="user-container">
-                                <img className="user-icon2" alt="" src="/user2.svg" />
-                                <div className="input-text-here2"><input type="email" placeholder="Enter your Password"/></div>
-                                <img className="user-icon2" alt="" src="/danger2.svg" />
-                            </div>
-                            {/* <div className="descriptive-text2 w-color">Descriptive text</div>
+              </div>
+            </div>
+            <div className="login-inputfield">
+              <div className="baseinputfield2">
+                <div className="title2 w-color">Password</div>
+                <div className="user-container">
+                  <img className="user-icon2" alt="" src="/user2.svg" />
+                  <div className="input-text-here2">
+                    <input type="email" placeholder="Enter your Password" />
+                  </div>
+                  <img className="user-icon2" alt="" src="/danger2.svg" />
+                </div>
+                {/* <div className="descriptive-text2 w-color">Descriptive text</div>
                             <div className="frame-div">
                                 <div className="option-1-frame">
                                     <div className="option-12">Option 1</div>
@@ -85,20 +115,19 @@ const Login = () => {
                                     <div className="option-12">Option 3</div>
                                 </div>
                             </div> */}
-                        </div>
-                    </div>
-                </div>
-                <div className="frame-wrapper">
-                    <div className="sign-up-wrapper">
-                        {/* <div className="sign-up-wrapper"> */}
-                            <button className="sign-up">Sign Up</button>
-                            {/* <div className="sign-up">Sign Up</div> */}
-                        {/* </div> */}
-                    </div>
-                </div>
+              </div>
             </div>
-
+          </div>
+          <div className="frame-wrapper">
+            <div className="sign-up-wrapper">
+              {/* <div className="sign-up-wrapper"> */}
+              <button className="sign-up">Sign Up</button>
+              {/* <div className="sign-up">Sign Up</div> */}
+              {/* </div> */}
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
